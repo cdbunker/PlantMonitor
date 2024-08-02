e22550a66bf4f5dbb5e6e8f2c42d4833b0389a16
cdbunker <cdbunker23@gmail.com>
2024-08-02 17:54:59 -0400

    bug

--- plant_monitor/client_app.py
+++ plant_monitor/client_app.py
@@ -4,8 +4,8 @@ from plant_monitor.client import Client
 
 
 def run():
-    client_config_path = os.path.join(CONFIGS_PATH, 'configs/client_config.yaml')
-    sensor_config_path = os.path.join(CONFIGS_PATH, 'configs/sensor_config.yaml')
+    client_config_path = os.path.join(CONFIGS_PATH, 'client_config.yaml')
+    sensor_config_path = os.path.join(CONFIGS_PATH, 'sensor_config.yaml')
     client = Client(client_config_path, sensor_config_path)
     client.run()
 
